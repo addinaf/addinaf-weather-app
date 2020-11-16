605081decb3ed1f80b667976e065f96fb21b7d9e
addinaf <74531322+addinaf@users.noreply.github.com>
2020-11-16 10:27:40 +0100

    copied from addinaf codesandbox
    all the files that I did (index html, css, js, icon, and img) from my codesandbox (addina.f.it) copied to vscode.

---
+++ src/index.js
@@ -0,0 +1,89 @@
+let dateElement = document.querySelector("#date");
+let currentDay = new Date();
+let date = currentDay.getDate();
+let month = currentDay.getMonth();
+let monthIndex = currentDay.getMonth();
+let months = [
+  "Jan",
+  "Feb",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "Aug",
+  "Sept",
+  "Oct",
+  "Nov",
+  "Dec"
+];
+
+let dayIndex = currentDay.getDay();
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday"
+];
+
+let year = currentDay.getFullYear();
+
+dateElement.innerHTML = `${days[dayIndex]} ${date} ${months[monthIndex]} ${year}`;
+
+let timeElement = document.querySelector("#time");
+let hours = currentDay.getHours();
+if (hours < 10) {
+  hours = `0${hours}`;
+}
+let minutes = currentDay.getMinutes();
+if (minutes < 10) {
+  minutes = `0${minutes}`;
+}
+
+timeElement.innerHTML = `${hours}:${minutes}`;
+
+function displayWeather(response) {
+  document.querySelector("#city").innerHTML = response.data.name.toUpperCase();
+  document.querySelector("#city-small").innerHTML = response.data.name;
+  let mainTemperature = Math.round(response.data.main.temp);
+  document.querySelector(
+    "#main-temperature"
+  ).innerHTML = `${mainTemperature}°C`;
+  document.querySelector("#humidity").innerHTML = response.data.main.humidity;
+  document.querySelector("#wind").innerHTML = Math.round(
+    response.data.wind.speed
+  );
+  document.querySelector("#real-feel").innerHTML = Math.round(
+    response.data.main.temp
+  );
+  document.querySelector("#max-temp").innerHTML = Math.round(
+    response.data.main.temp_max
+  );
+  document.querySelector("#min-temp").innerHTML = Math.round(
+    response.data.main.temp_min
+  );
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  let units = "metric";
+  let cityInput = document.querySelector("#city-input").value;
+  // let apiKey = "228eba262b4ca88b9a34ddd2463378be";
+  // let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
+  // axios.get(apiUrl).then(displayWeather);
+  searchCity(cityInput);
+}
+let searchForm = document.querySelector("#search-form");
+searchForm.addEventListener("submit", handleSubmit);
+
+function searchCity(city) {
+  let units = "metric";
+  let apiKey = "228eba262b4ca88b9a34ddd2463378be";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
+  axios.get(apiUrl).then(displayWeather);
+}
+
+searchCity("Milan");
